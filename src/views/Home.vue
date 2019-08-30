6490cd8a52273062e4dd160fc82b39c4343ef9c9
fastTofast <1024472173@qq.com>
2019-08-30 23:38:58 +0800

    增加按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <div class="class-nav"></div>
+    <div class="class-nav">
+      <div class="center-btn">
+        <span class="icon">+</span>
+        <span>众神归位</span>
+      </div>
+    </div>
     <div class="my-profile">
       <div class="profile-content">
         <div>cxp0923@qq.com</div>
@@ -23,7 +28,28 @@ export default {
   flex-direction: column;
   .class-nav {
     height: 60vh;
+    position: relative;
     background-color: #19191b;
+    .center-btn {
+      height: 20vh;
+      width: 20vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      background-color: #af4b4b;
+      cursor: pointer;
+      border-radius: 50%;
+      position: absolute;
+      margin: 0 auto;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      span.icon {
+        font-size: 80px;
+        color: #ffffff;
+      }
+    }
   }
   .my-profile {
     height: 35vh;
@@ -34,6 +60,7 @@ export default {
     .profile-content {
       width: 80%;
       height: 70%;
+      color: #ffffff;
       background-color: #af4b4b;
       border-radius: 8px;
       display: flex;
